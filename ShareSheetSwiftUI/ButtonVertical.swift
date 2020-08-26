cec9bb1cb4480a1c60e9109895642c53f9d76ea7
FELIX <felixkautz@gmail.com>
2020-08-26 18:18:51 +0300

    Кнопка ShareSheet Окно Sheet

---
+++ ShareSheetSwiftUI/ButtonVertical.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonVertical.swift
+//  ShareSheetSwiftUI
+//
+//  Created by Felix Falkovsky on 26.08.2020.
+//
+
+import SwiftUI
+
+struct ButtonVertical: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonVertical_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonVertical()
+    }
+}
