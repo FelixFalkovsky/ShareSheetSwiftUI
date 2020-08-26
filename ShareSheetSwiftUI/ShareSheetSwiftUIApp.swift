3dd113bb18b0adce450dd8319a5495a3c70f639a
FELIX <felixkautz@gmail.com>
2020-08-26 13:42:14 +0300

    Initial Commit

---
+++ ShareSheetSwiftUI/ShareSheetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheetSwiftUIApp.swift
+//  ShareSheetSwiftUI
+//
+//  Created by Felix Falkovsky on 26.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
